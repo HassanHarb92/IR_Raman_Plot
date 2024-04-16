19f10f9b64c13d1b75d2414e5067efb490893e01
Hassan <hharb@anl.gov>
2024-04-16 16:21:09 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,54 +4,51 @@ import numpy as np
 import re
 
 def parse_gaussian_log(contents):
-    """ Parses Gaussian log file contents to extract frequencies, IR and Raman intensities. """
+    """
+    Parses Gaussian log file contents to extract frequencies and IR intensities.
+    """
     freq_pattern = r"Frequencies -- ([\d\.\- ]+)"
-    ir_intensity_pattern = r"IR Inten -- ([\d\.\- ]+)"
-    raman_intensity_pattern = r"Raman Activ -- ([\d\.\- ]+)"
-
-    frequencies, ir_intensities, raman_intensities = [], [], []
-    n_atoms = 0
+    ir_intensity_pattern = r"IR Inten"
+    frequencies, ir_intensities = [], []
 
     for line in contents.splitlines():
-        if "NAtoms=" in line:
-            n_atoms = int(line.split('=')[1])
-        if re.search(freq_pattern, line):
-            frequencies.extend([float(f) for f in re.search(freq_pattern, line).group(1).split()])
-        if re.search(ir_intensity_pattern, line):
-            ir_intensities.extend([float(i) for i in re.search(ir_intensity_pattern, line).group(1).split()])
-        if re.search(raman_intensity_pattern, line):
-            raman_intensities.extend([float(r) for r in re.search(raman_intensity_pattern, line).group(1).split()])
+        if "Frequencies --" in line:
+            freq_matches = re.search(freq_pattern, line)
+            if freq_matches:
+                frequencies.extend([float(f) for f in freq_matches.group(1).split()])
+        elif "IR Inten" in line:
+            words = line.split()
+            for i in range(3,len(words)):
+                ir_intensities.append(float(words[i]))    
 
-    return frequencies, ir_intensities, raman_intensities, n_atoms
+
+    return frequencies, ir_intensities
 
 def plot_spectrum(frequencies, intensities, title, color):
-    """ Plots a spectrum given frequencies and intensities. """
-    if not intensities:
-        st.write(f"No data for {title}")
-        return
-
+    """
+    Plots a spectrum given frequencies and intensities.
+    """
     plt.figure(figsize=(10, 4))
-    plt.plot(frequencies, intensities, color=color)
+    plt.stem(frequencies, intensities, linefmt=color, markerfmt=' ', basefmt=" ")
     plt.title(f"{title} Spectrum")
-    plt.xlabel("Frequency (cm-1)")
+    plt.xlabel("Frequency (cm^-1)")
     plt.ylabel("Intensity")
     plt.grid(True)
     st.pyplot(plt)
 
 # Streamlit UI components
-st.title('Gaussian .log File Analyzer')
+st.title('Gaussian .log File IR Spectrum Analyzer')
 uploaded_file = st.file_uploader("Upload your Gaussian .log file", type="log")
-color_ir = st.color_picker("Choose a color for IR Spectrum", "#FF0000")
-color_raman = st.color_picker("Choose a color for Raman Spectrum", "#0000FF")
+color = st.color_picker("Choose a color for the IR Spectrum plot", "#FF0000")
+
 
 if uploaded_file is not None:
     contents = uploaded_file.getvalue().decode("utf-8")
-    frequencies, ir_intensities, raman_intensities, n_atoms = parse_gaussian_log(contents)
-
-    if n_atoms == 0:
-        st.error("No atomic data found in file.")
+    frequencies, ir_intensities = parse_gaussian_log(contents)
+    print("frequencies", frequencies)
+    print("IR intensities",ir_intensities)
+    if frequencies and ir_intensities:
+        plot_spectrum(frequencies, ir_intensities, "IR", color)
     else:
-        st.write(f"Number of atoms: {n_atoms}")
-        plot_spectrum(frequencies, ir_intensities, "IR", color_ir)
-        plot_spectrum(frequencies, raman_intensities, "Raman", color_raman)
+        st.error("No IR frequency or intensity data found in the file.")
 
